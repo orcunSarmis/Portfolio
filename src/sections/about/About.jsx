5cab6ed9b102946a20e17e67b532eb65fc368135
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-07-01 21:25:26 +1000

    adding folders structure

---
+++ src/sections/about/About.jsx
@@ -0,0 +1,9 @@
+import './about.css'
+
+const About = () => {
+  return (
+	<section id="about"><h2>about</h2></section>
+  )
+}
+
+export default About
---
+++ src/sections/faqs/FAQS.jsx
@@ -0,0 +1,9 @@
+import './faqs.css'
+
+const FAQS = () => {
+  return (
+	<section id="faqs"><h2>FAQS</h2></section>
+  )
+}
+
+export default FAQS
