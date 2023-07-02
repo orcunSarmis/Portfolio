38460f4e5abeef82d737711677f20fed76322bdd
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-07-02 17:35:17 +1000

    adding styling of nav

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,17 +1,21 @@
 import Logo from '../../assets/logo.jpg'
+import data from './data'
+import {IoIosColorPalette} from 'react-icons/io'
 import './navbar.css'
 
 const Navbar = () => {
   return (
 	<nav>
 		<div className="container nav__container">
-			<a href="">
+			<a href="index.html" className='nav__logo'>
 				<img src={Logo} alt="Logo" />
 			</a>
 			<ul className='nav__menu'>
-				
+				{
+					data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+				}
 			</ul>
-			<button></button>
+			<button id='theme__icon'><IoIosColorPalette/></button>
 		</div>
 	</nav>
   )
