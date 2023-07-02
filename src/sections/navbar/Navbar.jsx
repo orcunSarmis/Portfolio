b142d80213dd9966b16ce6d52ba50681d4aab06e
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-07-02 12:33:37 +1000

    adding data

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,19 @@
+import Logo from '../../assets/logo.jpg'
 import './navbar.css'
 
 const Navbar = () => {
   return (
-	<nav><h2>navbar</h2></nav>
+	<nav>
+		<div className="container nav__container">
+			<a href="">
+				<img src={Logo} alt="Logo" />
+			</a>
+			<ul className='nav__menu'>
+				
+			</ul>
+			<button></button>
+		</div>
+	</nav>
   )
 }
 
