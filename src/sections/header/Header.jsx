14111649b6a9dc9c3223bc19f8c28889fab687ae
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-07-02 17:44:58 +1000

    adding about styling

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,9 +1,27 @@
-import './header.css'
+import HeaderImage from "../../assets/header.jpg";
+import "./header.css";
 
 const Header = () => {
   return (
-	<header id="header"><h2>header</h2></header>
-  )
-}
+    <header id="header">
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Header Image" />
+        </div>
+        <h3>Orcun Sarmis</h3>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis est
+          pariatur molestias assumenda! Aspernatur dolorum consequuntur mollitia
+          ipsum numquam maiores, animi quidem molestias aliquid similique
+          tenetur. Voluptatibus rerum facilis explicabo.DC
+        </p>
+        <div className="header__cta">
+          <a href="#contact">Let's Talk</a>
+          <a href="#portfolio">My Work</a>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
