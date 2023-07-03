076340a575fecb422e8f47004cfa6cbab73bd62a
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-07-03 16:55:31 +1000

    adding cv

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,9 +1,36 @@
-import './about.css'
+import AboutImage from "../../assets/about.jpg";
+import CV from '../../assets/'
+import "./about.css";
 
 const About = () => {
   return (
-	<section id="about"><h2>about</h2></section>
-  )
-}
+    <section id="about">
+      <div className="container about__container">
+        <div className="about__left">
+          <div className="about__portrait">
+            <img src={AboutImage} alt="" />
+          </div>
+        </div>
+        <div className="about__right">
+          <h2>About Me</h2>
+          <div className="about__cards"></div>
+        </div>
+        <p>
+          Building projects my clients love have always been my passion. Being
+          in the web development industry for over 2 years and serving more than
+          70 happy clients worldwide, I'm always motivated to do more!
+        </p>
+        <p>
+          Hi, my name is Orcun Sarmis from Brisbane, Australia. I'm a full-stack web
+          developer with a Bachelors degree in Computer Science. My top priority
+          is to get your business online the right way, giving you
+          industry-standard design and all the functionality you need to operate
+          smoothly online. Get in touch today with the details of your project
+          let's get started! Check out my resume below!
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
