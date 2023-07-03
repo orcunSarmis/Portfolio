90a49ed7e2acd162ad743217b8039c62bc0ae273
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-07-03 20:37:28 +1000

    adding projects

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,9 +1,24 @@
-import './portfolio.css'
+import Projects from "./Projects";
+import ProjectsCategories from "./ProjectsCategories";
+import data from './data'
+import "./portfolio.css";
+import { useState } from "react";
 
 const Portfolio = () => {
+	const [projects, setProjects] = useState(data);
   return (
-	<section id="portfolio"><h2>portfolio</h2></section>
-  )
-}
+    <section id="portfolio">
+      <h2>Recent Projects</h2>{" "}
+      <p>
+        Check out some of the projects I recently worked on for my clients. Use
+        the buttons to toggle the different categories.
+      </p>
+      <div className="container portfolio__container">
+        <ProjectsCategories />
+        <Projects projects={projects} />
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
