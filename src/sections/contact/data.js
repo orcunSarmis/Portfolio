8d053289384140df14c6fed034cd31d4a79dc8e2
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-07-06 20:20:06 +1000

    adding clean up

---
+++ src/sections/contact/data.js
@@ -0,0 +1,17 @@
+import {HiOutlineMail} from 'react-icons/hi'
+import {ImLinkedin} from 'react-icons/im'
+import {FaWhatsapp} from 'react-icons/fa'
+
+const data = [
+    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:yorcunsarmis@gmail.com'},
+    {id: 2, icon: <ImLinkedin/>, link: 'https://www.linkedin.com/in/yorcun-sarmis/'},
+    // {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/+233557097546'}
+]
+
+
+// alternative whatsApp link
+// https://wa.me/yournumber
+// https://api.whatsapp.com/send/?phone=%2B233557097546
+
+
+export default data
--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,12 +1,14 @@
-import {AiOutlineInstagram} from 'react-icons/ai'
+// import {AiOutlineInstagram} from 'react-icons/ai'
 import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiOutlineDribbble} from 'react-icons/ai'
+import {ImLinkedin} from 'react-icons/im'
+// import {AiOutlineDribbble} from 'react-icons/ai'
 import {AiFillGithub} from 'react-icons/ai'
 
 const data = [
-    {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
+    // {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
     {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
+	{id: 3, icon: <ImLinkedin/>, link: 'https://www.linkedin.com/in/yorcun-sarmis/'},
+    // {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
     {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
 ]
 
