fb25d2c8ab91160353f781750b1586a78e333ae6
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-07-17 21:24:35 +1000

    project completed

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,22 +1,29 @@
+import { useEffect } from "react";
 import HeaderImage from "../../assets/header.jpg";
 import data from './data';
+import AOS from "aos";
+import 'aos/dist/aos.css'
 import "./header.css";
 
 const Header = () => {
+	useEffect(() => {
+		AOS.init({duration: 2000})
+	}, [])
+
   return (
     <header id="header">
       <div className="container header__container">
-        <div className="header__profile">
+        <div className="header__profile" data-aos="fade-in">
           <img src={HeaderImage} alt="Header Image" />
         </div>
-        <h3>Orcun Sarmis</h3>
-        <p>
+        <h3 data-aos="fade-up">Orcun Sarmis</h3>
+        <p data-aos="fade-up">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis est
           pariatur molestias assumenda! Aspernatur dolorum consequuntur mollitia
           ipsum numquam maiores, animi quidem molestias aliquid similique
           tenetur. Voluptatibus rerum facilis explicabo.DC
         </p>
-        <div className="header__cta">
+        <div className="header__cta" data-aos="fade-up">
           <a href="#contact" className="btn primary">Let's Talk</a>
           <a href="#portfolio" className="btn light">My Work</a>
         </div>
