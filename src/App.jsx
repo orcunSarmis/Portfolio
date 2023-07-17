76ee4780a75aaf6fbf0b1358162e4c2d1c43d6d8
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-07-17 20:58:16 +1000

    adding floating nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,43 @@ import FloatingNav from './sections/floating-nav/FloatingNav';
 import Modal from './components/Modal';
 import Theme from './theme/Theme';
 import { useThemeContext } from './context/theme-context';
+import { useRef, useState, useEffect } from 'react';
 
 
 const App = () => {
-	const { themeState } = useThemeContext();
+	const {themeState} = useThemeContext();
+
+	const mainRef = useRef();
+	const [showFloatingNav, setShowFloatingNav] = useState(true);
+	const [siteYPostion, setSiteYPosition] = useState(0)
+
+	const showFloatingNavHandler = () => {
+	  setShowFloatingNav(true);
+	}
+
+	const hideFloatingNavHandler = () => {
+	  setShowFloatingNav(false);
+	}
+
+	// check if floating nav should be shown or hidden
+	const floatingNavToggleHandler = () => {
+	  // check if we scrolled up or down at least 20px
+	  if(siteYPostion < (mainRef?.current?.getBoundingClientRect().y - 20) || siteYPostion > (mainRef?.current?.getBoundingClientRect().y + 20)) {
+		showFloatingNavHandler();
+	  } else {
+		hideFloatingNavHandler();
+	  }
+
+	  setSiteYPosition(mainRef?.current?.getBoundingClientRect().y);
+	}
+
+	useEffect(() => {
+	  const checkYPosition = setInterval(floatingNavToggleHandler, 2000);
+
+	  // cleanup function
+	  return () => clearInterval(checkYPosition);
+	}, [siteYPostion]
+	)
 
   return (
 	<main className={`${themeState.primary} ${themeState.background}`}>
@@ -28,8 +61,7 @@ const App = () => {
 		<Contact />
 		<Footer />
 		<Theme />
-		{/* <FloatingNav /> */}
-		{/* <Modal /> */}
+		{ showFloatingNav && <FloatingNav />}
 	</main>
   )
 }
